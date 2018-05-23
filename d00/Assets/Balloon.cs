8ec6503e8d3bde333f6cfcae169e88b241b054a4
Geoffrey NEGRI <gnegri@e3r11p15.42.fr>
2018-05-23 19:03:36 +0200

    Updated ex00 with limited inputs and destroying GameObject

--- d00/Assets/Balloon.cs
+++ d00/Assets/Balloon.cs
@@ -9,15 +9,23 @@ public class Balloon : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		souffle = 0;
+		souffle_max = 18;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("space"))
-        	balloon.transform.localScale += new Vector3 (0.1f, 0.1f, 0.1f);
-		else if (balloon.transform.localScale != new Vector3 (0, 0, 0))
-			balloon.transform.localScale -= new Vector3 (0.05f, 0.05f, 0.05f);
-		
+		if (Input.GetKeyDown("space") && souffle != souffle_max)
+		{
+        	balloon.transform.localScale += new Vector3 (0.07f, 0.07f, 0.07f);
+			souffle += 1;
+		}
+		else if (balloon.transform.localScale.x > 0.01 && balloon.transform.localScale.y > 0.01)
+			balloon.transform.localScale -= new Vector3 (0.004f, 0.004f, 0.004f);
+		if ((balloon.transform.localScale.x > 0.7 && balloon.transform.localScale.y > 0.7) || (balloon.transform.localScale.x <= 0.01 && balloon.transform.localScale.y <= 0.01))
+		{
+			Destroy(balloon);
+			Debug.Log(Mathf.RoundToInt(Time.time));
+		}
 	}
 }
