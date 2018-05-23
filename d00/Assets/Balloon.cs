b9d72cff8dc5498618af7fb52f9a446080bf0b8c
Geoffrey NEGRI <gnegri@e3r11p15.42.fr>
2018-05-23 17:13:23 +0200

    Began d00. Added balon Gameobject and script

---
+++ d00/Assets/Balloon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour {
+	public GameObject	balloon;
+	public int			souffle;
+	private int			souffle_max;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space"))
+        	balloon.transform.localScale += new Vector3 (0.1f, 0.1f, 0.1f);
+		else if (balloon.transform.localScale != new Vector3 (0, 0, 0))
+			balloon.transform.localScale -= new Vector3 (0.05f, 0.05f, 0.05f);
+		
+	}
+}
