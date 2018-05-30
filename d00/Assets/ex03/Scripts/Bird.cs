e2434551bb72181503ee827009df3465e4f280af
Geoffrey NEGRI <gnegri@e3r12p14.42.fr>
2018-05-30 11:48:54 +0200

    Added ex03 flappy bird with collision and score

---
+++ d00/Assets/ex03/Scripts/Bird.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+	public GameObject	bird;
+	public GameObject	pipe;
+	public float up;
+	public float down;
+	public int	score;
+	public bool	lose;
+	public float		speed;
+
+	// Use this for initialization
+	void Start () {
+		lose = false;
+		speed = 0.08f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bird.transform.position.y >= -3.3 && !lose) {
+			bird.transform.Translate(Vector3.down * down);		
+			if (Input.GetKeyDown("space"))
+				bird.transform.Translate(Vector3.up * up);
+		}
+		else
+		{
+			lose = true;
+		}
+	}
+
+	void	OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "pipe")
+			lose = true;
+	}
+
+	void	OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "pipe")
+			{
+				score += 5;
+				Debug.Log("Score: " + score);
+				speed += 0.015f;
+			}
+	}
+}
---
+++ d00/Assets/ex03/Scripts/Pipe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+	public GameObject	bird;
+	public GameObject	pipe;
+	public float		begin;
+	public float		end;
+
+	private Bird	bird_script;
+	private int		score;
+	private Vector3	temp;
+	// Use this for initialization
+	void Start () {
+		pipe = gameObject;
+		bird_script = bird.GetComponent<Bird>();
+		score = bird_script.score;
+		temp = pipe.transform.position;
+		temp.x = begin;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!bird_script.lose) {
+			pipe.transform.Translate(Vector3.left * bird_script.speed);
+			if (pipe.transform.position.x <= end)
+				pipe.transform.position = temp;
+		}
+	}
+}
