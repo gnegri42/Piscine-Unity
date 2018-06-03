c9bbbb2ee19e6237786bd6ced65e4b46b508771a
Geoffrey Negri <gnegri@student.42.fr>
2018-06-03 21:48:19 +0200

    Added second city

--- d02/Assets/Scripts/City.cs
+++ d02/Assets/Scripts/City.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class City : MonoBehaviour {
 	public int 			life = 10;
 	public GameObject	citizen;
+	public bool			townHall;
+	public Vector3		spawnPosition = new Vector3(0, 0, 0);
 
 	// Use this for initialization
 	void Start () {
-		
+		if (townHall)
+			InvokeRepeating("SpawnCitizen", 2.0f, 10.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void SpawnCitizen() {
+		Instantiate(citizen, spawnPosition, Quaternion.identity);
 	}
 }
