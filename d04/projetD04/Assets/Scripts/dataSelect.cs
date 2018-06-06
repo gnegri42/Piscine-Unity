c1ff09cbd2cb29a1d5371471e002d79b823bcf63
Geoffrey NEGRI <gnegri@e3r12p14.42.fr>
2018-06-06 19:23:46 +0200

    Finished LevelSelection menu and added variables to playerPrefs

---
+++ d04/projetD04/Assets/Scripts/dataSelect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dataSelect : MonoBehaviour {
+	public int 	selectedLevel = 0;
+	public List<GameObject> levelList;
+	public int 	totalLevels = 4;
+	public Text levelName;
+	public Text lifesNumber;
+	public Text ringsNumber;
+	public Text levelScore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			if (selectedLevel < totalLevels - 1)
+				selectedLevel++;
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			if (selectedLevel > 0)
+				selectedLevel--;
+		}
+		transform.position = levelList[selectedLevel].transform.position;
+		levelName.text = levelList[selectedLevel].name;
+		SetTextLifesNumber();
+		SetTextRingsNumber();
+		SetTextLevelScore();
+	}
+
+	void SetTextLifesNumber () {
+		lifesNumber.text = PlayerPrefs.GetInt("lostLives").ToString();
+	}
+	void SetTextRingsNumber () {
+		ringsNumber.text = PlayerPrefs.GetInt("rings").ToString();
+	}
+	void SetTextLevelScore () {
+		levelScore.text = PlayerPrefs.GetInt("lvl" + selectedLevel + "Score").ToString();
+	}
+}
--- d04/projetD04/Assets/Scripts/userPrefs.cs
+++ d04/projetD04/Assets/Scripts/userPrefs.cs
@@ -2,24 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class userPrefs : MonoBehaviour {
 	public InputField		input;
-	public int				lifes;
-	public int				rings;
-	public int				score;
+
 	// Use this for initialization
 	void Start () {
 		if (PlayerPrefs.GetString("user") != null)
 			input.text = PlayerPrefs.GetString("user");
-	// Ce profil doit contenir la liste des niveaux
-	// débloqués par le joueur, le nombre de vies qu’il a perdu toutes parties confondues,
-	// le nombre d’anneaux gagnés toutes parties confondues ainsi que son meilleur score de run
-	// sur chaque niveau.
 	}
 	
 	public void Update() {
+		if (Input.GetKeyDown(KeyCode.Return))
+			SceneManager.LoadScene("DataSelect", LoadSceneMode.Additive);
 		PlayerPrefs.SetString("user", input.text);
+		PlayerPrefs.SetInt("lostLives", 0);
+		PlayerPrefs.SetInt("rings", 0);
+		PlayerPrefs.SetInt("maxLevel", 0);
+		PlayerPrefs.SetInt("lvl0Score", 0);
+		PlayerPrefs.SetInt("lvl1Score", 0);
+		PlayerPrefs.SetInt("lvl2Score", 0);
+		PlayerPrefs.SetInt("lvl3Score", 0);
 	}
 
 	//Save prefs
